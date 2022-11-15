7fe8a72c53ab41c4782c38f4c2826b68d1f9f9a0
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-15 23:43:17 +0200

    02. SoftUni Party

---
+++ src/L03_SetsAndMapsAdvanced/E02_SoftUniParty.java
@@ -0,0 +1,25 @@
+package L03_SetsAndMapsAdvanced;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class E02_SoftUniParty {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        String input;
+        Set<String> guests = new TreeSet<>();
+        
+        while (!"PARTY".equals(input = console.nextLine())){
+            guests.add(input);
+        }
+        while (!"END".equals(input = console.nextLine())){
+            guests.remove(input);
+        }
+        System.out.println(guests.size());
+        for (String guest : guests) {
+            System.out.println(guest);
+        }
+    }
+}
