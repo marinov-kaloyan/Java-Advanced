54da7a8fa648e57d3eaf6283391964e48103ffeb
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-15 23:43:41 +0200

    04. Count Real Numbers

---
+++ src/L03_SetsAndMapsAdvanced/E04_CountRealNumbers.java
@@ -0,0 +1,22 @@
+package L03_SetsAndMapsAdvanced;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class E04_CountRealNumbers {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        double[] numbers = Arrays
+                .stream(console.nextLine().split("\\s+"))
+                .mapToDouble(Double::parseDouble)
+                .toArray();
+        HashMap<Double, Integer> countOfNumbers = new LinkedHashMap<>();
+        for (double number : numbers) {
+            countOfNumbers.putIfAbsent(number, 0);
+            countOfNumbers.put(number, countOfNumbers.get(number)+1);
+        }
+        countOfNumbers.forEach((key, value) -> System.out.printf("%.1f -> %d%n", key, value));
+    }
+}
