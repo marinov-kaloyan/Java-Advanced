c63bbf005f2f0d9fefb6666c64fa836f079d22ac
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-15 23:44:21 +0200

    06. Product Shop

---
+++ src/L03_SetsAndMapsAdvanced/E06_ProductShop.java
@@ -0,0 +1,38 @@
+package L03_SetsAndMapsAdvanced;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+public class E06_ProductShop {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String input;
+        Map <String, LinkedHashMap<String, Double>> shops = new TreeMap<>();
+        while (!"Revision".equals(input= console.nextLine())){
+            String[] attribute = input.split(",\\s+");
+            String shop = attribute[0];
+            String product = attribute[1];
+            double price = Double.parseDouble(attribute[2]);
+            shops.putIfAbsent(shop, new LinkedHashMap<>());
+            shops.get(shop).put(product, price);
+        }
+        System.out.println(shops.entrySet()
+                .stream()
+                .map(shop ->
+                        String.format("%s->%n%s", shop.getKey(),
+                                shop.getValue()
+                                        .entrySet()
+                                        .stream()
+                                        .map(product ->
+                                                String.format("Product: %s, Price: %.1f",
+                                                        product.getKey(),
+                                                        product.getValue()))
+                                        .collect(Collectors.joining(System.lineSeparator()))
+                        )
+                )
+                .collect(Collectors.joining(System.lineSeparator())));
+    }
+}
