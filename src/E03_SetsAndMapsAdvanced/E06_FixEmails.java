27a017f92aa66e758ebc2d37578897415fe2751e
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-17 23:44:12 +0200

    06. Fix Emails

---
+++ src/E03_SetsAndMapsAdvanced/E06_FixEmails.java
@@ -0,0 +1,21 @@
+package E03_SetsAndMapsAdvanced;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class E06_FixEmails {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String input;
+        Map<String, String> emails = new LinkedHashMap<>();
+        while (!"stop".equals(input = console.nextLine())) {
+            String email = console.nextLine();
+            String domain = email.substring(email.lastIndexOf(".")+1);
+            if (!("us".equals(domain) || "uk".equals(domain) || "com".equals(domain))) {
+                emails.put(input, email);
+            }
+        }
+        emails.forEach((k, v) -> System.out.println(k + " -> " + v));
+    }
+}
