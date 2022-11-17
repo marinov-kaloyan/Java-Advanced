30f7588c3816000dc83a09c9292c8ab5a6c885f4
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-17 23:42:32 +0200

    03. Periodic Table

---
+++ src/E03_SetsAndMapsAdvanced/E03_PeriodicTable.java
@@ -0,0 +1,19 @@
+package E03_SetsAndMapsAdvanced;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class E03_PeriodicTable {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int lineToInput = Integer.parseInt(console.nextLine());
+        Set<String> chemicalElements = new TreeSet<>();
+        for (int i=0; i< lineToInput; i++){
+            String[] elements = console.nextLine().split("\\s+");
+            chemicalElements.addAll(Arrays.asList(elements));
+        }
+        System.out.println(String.join(" ", chemicalElements));
+    }
+}
