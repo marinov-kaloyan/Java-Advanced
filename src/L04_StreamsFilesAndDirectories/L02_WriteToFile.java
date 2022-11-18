de2619e5ed6cffb42e3f7a3ccdbe62fdbf8bb29e
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-18 23:28:59 +0200

    02. Write to File

---
+++ src/L04_StreamsFilesAndDirectories/L02_WriteToFile.java
@@ -0,0 +1,27 @@
+package L04_StreamsFilesAndDirectories;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class L02_WriteToFile {
+    public static void main(String[] args) throws IOException {
+        String inPath = "D:\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+        String outputPath = "D:\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\02.WriteToFileOutput.txt";
+        Set<Character> punctuations = new HashSet<>();
+        Collections.addAll(punctuations, '.', ',', '!', '?');
+
+        try (InputStream in = new FileInputStream(inPath);
+             OutputStream out = new FileOutputStream(outputPath)) {
+            int oneByte;
+            while ((oneByte = in.read()) >= 0) {
+                if(!punctuations.contains((char) oneByte)){
+                    out.write(oneByte);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
