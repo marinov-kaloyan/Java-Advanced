fb57d489ef73a1960e633eb84099d3e49462edaa
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-19 23:52:30 +0200

    08. Get Folder Size

---
+++ src/E04_StreamsFilesAndDirectories/E08_GetFolderSize.java
@@ -0,0 +1,30 @@
+package E04_StreamsFilesAndDirectories;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Objects;
+
+public class E08_GetFolderSize {
+    public static void main(String[] args) throws IOException {
+        File folder = new File("D:\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources");
+        Deque<File> dirs = new ArrayDeque<>();
+        dirs.offer(folder);
+
+        int sumBytes = 0;
+        while (!dirs.isEmpty()) {
+            File current = dirs.poll();
+            File[] nestedFiles = current.listFiles();
+            for (File f : Objects.requireNonNull(nestedFiles)) {
+                if (f.isDirectory()) {
+                    dirs.offer(f);
+                }
+                else {
+                    sumBytes += f.length();
+                }
+            }
+        }
+        System.out.println("Folder size: " + sumBytes);
+    }
+}
