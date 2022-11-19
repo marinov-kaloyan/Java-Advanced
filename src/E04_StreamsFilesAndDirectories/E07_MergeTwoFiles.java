35a1819512f2a3987a78a3124a7056c77ac7c3b7
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-19 23:52:20 +0200

    07. Merge Two Files

---
+++ src/E04_StreamsFilesAndDirectories/E07_MergeTwoFiles.java
@@ -0,0 +1,21 @@
+package E04_StreamsFilesAndDirectories;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class E07_MergeTwoFiles {
+    public static void main(String[] args) throws IOException {
+        Path inputOne = Paths.get("D:\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputOne.txt");
+        Path inputTwo = Paths.get("D:\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputTwo.txt");
+        Path result = Paths.get("D:\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\result.txt");
+
+        List<String> linesInFiles = new ArrayList<>();
+        linesInFiles.addAll(Files.readAllLines(inputOne));
+        linesInFiles.addAll(Files.readAllLines(inputTwo));
+        Files.write(result, linesInFiles);
+    }
+}
