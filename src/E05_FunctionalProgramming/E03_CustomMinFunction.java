a73256d1ea25b8e8c382940fa78b9b31c3486823
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-20 23:18:34 +0200

    03. Custom Min Function

---
+++ src/E05_FunctionalProgramming/E03_CustomMinFunction.java
@@ -0,0 +1,13 @@
+package E05_FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class E03_CustomMinFunction {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        Function<Integer[], Integer> maxElement = intArray -> Arrays.stream(intArray).sorted().findFirst().orElse(0);
+        System.out.println(maxElement.apply(Arrays.stream(console.nextLine().split("\\s+")).map(Integer::parseInt).toArray(Integer[]::new)));
+    }
+}
