f33c2fcd6575621b9db72d0e09ff3c71ae761d1a
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-20 23:19:08 +0200

    06. Predicate For Names

---
+++ src/E05_FunctionalProgramming/E06_PredicateForNames.java
@@ -0,0 +1,19 @@
+package E05_FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class E06_PredicateForNames {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int nameLength = Integer.parseInt(console.nextLine());
+        List <String> names = Arrays.stream(console.nextLine().split("\\s+")).collect(Collectors.toList());
+        Predicate <String> nameToPrint = s -> s.length()<=nameLength;
+        Consumer<String> print = System.out::println;
+        names.stream().filter(nameToPrint).forEach(print);
+    }
+}
