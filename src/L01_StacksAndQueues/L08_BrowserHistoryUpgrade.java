f23aa72cb34ebdfbf1a26a38c3a9b746d08232b1
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-21 10:55:17 +0200

    08. Browser History Upgrade

---
+++ src/L01_StacksAndQueues/L08_BrowserHistoryUpgrade.java
@@ -0,0 +1,39 @@
+package L01_StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class L08_BrowserHistoryUpgrade {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String input;
+        String currentUrl = "";
+        ArrayDeque<String> backWebSites = new ArrayDeque<>();
+        ArrayDeque<String> forwardWebSites = new ArrayDeque<>();
+        while (!"Home".equals(input = console.next())) {
+            switch (input) {
+                case "back":
+                    if (backWebSites.size() > 1) {
+                        forwardWebSites.addFirst(currentUrl);
+                        System.out.println(currentUrl = backWebSites.pop());
+                    } else {
+                        System.out.println("no previous URLs");
+                    }
+                    break;
+                case "forward":
+                    if (forwardWebSites.size() > 0) {
+                        backWebSites.push(currentUrl);
+                        System.out.println(currentUrl = forwardWebSites.pop());
+                    } else {
+                        System.out.println("no next URLs");
+                    }
+                    break;
+                default:
+                    backWebSites.push(currentUrl);
+                    currentUrl = input;
+                    forwardWebSites.clear();
+                    System.out.println(currentUrl);
+            }
+        }
+    }
+}
