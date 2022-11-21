2c625c44e731e698b976a4c1966f959a82efa7f8
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-21 12:04:35 +0200

    02. Constructors

---
+++ src/L06_DefiningClasses/L02 Constructors/Main.java
@@ -0,0 +1,20 @@
+package L06_DefiningClasses;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        Set<Car> cars = new LinkedHashSet<>();
+        int lineNumber = Integer.parseInt(console.nextLine());
+        while (lineNumber-- > 0) {
+            String[] token = console.nextLine().split("\\s+");
+            Car car = token.length == 1 ? new Car(token[0]) : new Car(token[0], token[1],  Integer.parseInt(token[2]));
+            cars.add(car);
+        }
+        cars.forEach(car -> System.out.println(car.carInfo()));
+    }
+}
