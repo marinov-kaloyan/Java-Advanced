6e9a7e5a21e91f3e99bc7b4f994ff97215cbab04
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-26 22:36:17 +0200

    01. Generic Box

---
+++ E07_Generics/src/E01_GenericBox/Box.java
@@ -0,0 +1,14 @@
+package E01_GenericBox;
+
+public class Box<E> {
+    private E element;
+
+    public Box(E element) {
+        this.element = element;
+    }
+
+    @Override
+    public String toString() {
+        return element.getClass().getName() + ": " + element;
+    }
+}
