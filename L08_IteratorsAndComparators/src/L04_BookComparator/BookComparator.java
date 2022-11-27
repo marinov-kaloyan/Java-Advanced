7c6fc4e6612eb76333cc43773afc2a33119ed1d0
Arraxiel <kaloyanmarinov93@gmail.com>
2022-11-27 22:24:19 +0200

    04. Book Comparator

---
+++ L08_IteratorsAndComparators/src/L04_BookComparator/BookComparator.java
@@ -0,0 +1,10 @@
+package L04_BookComparator;
+
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book first, Book second) {
+        return first.compareTo(second);
+    }
+}
